748c31668e08ed2dc5920739d27a8a97e1a0787d
Eduard Tarassov <eduardtarassov@gmail.com>
2014-02-25 13:38:37 +0000

    Finished Day 5!

--- ZombieBird/src/com/eduardtarassov/gameworld/GameRenderer.java
+++ ZombieBird/src/com/eduardtarassov/gameworld/GameRenderer.java
@@ -1,5 +1,7 @@
 package com.eduardtarassov.gameworld;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
@@ -24,5 +26,43 @@ public class GameRenderer {
 
     public void render(){
         System.out.println("GameRenderer - render");
+
+        //We draw a black background. This prevents flickering.
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+
+        /*
+         * 2. We draw the Filled rectangle
+         */
+
+        // Tells shapeRenderer to begin drawing filled shapes
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        // Chooses RGB Color of 87, 109, 120 at full opacity
+        shapeRenderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Draws the rectangle from myWorld (Using ShapeType.Filled)
+        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
+                myWorld.getRect().width, myWorld.getRect().height);
+
+        // Tells the shapeRenderer to finish rendering
+        // We MUST do this every time.
+        shapeRenderer.end();
+
+        /*
+         * 3. We draw the rectangle's outline
+         */
+
+        // Tells shapeRenderer to draw an outline of the following shapes
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        // Chooses RGB Color of 255, 109, 120 at full opacity
+        shapeRenderer.setColor(255 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+
+        // Draws the rectangle from myWorld (Using ShapeType.Line)
+        shapeRenderer.rect(myWorld.getRect().x, myWorld.getRect().y,
+                myWorld.getRect().width, myWorld.getRect().height);
+
+        shapeRenderer.end();
     }
 }
--- ZombieBird/src/com/eduardtarassov/gameworld/GameWorld.java
+++ ZombieBird/src/com/eduardtarassov/gameworld/GameWorld.java
@@ -1,11 +1,25 @@
 package com.eduardtarassov.gameworld;
 
+import com.badlogic.gdx.math.Rectangle;
+
 /**
  * Created by Eduard on 2/25/14.
  * This class is responsible for updating our game objects.
  */
 public class GameWorld {
+
+    private Rectangle rect = new Rectangle(0, 0, 17, 12);  // Parameters are: X Coordinates, Y Coordinates, Width, Height
+
+
     public void update(float delta) {
         System.out.println("GameWorld - update");
+        rect.x++;
+        if (rect.x > 137) {
+            rect.x = 0;
+        }
+    }
+
+    public Rectangle getRect() {
+        return rect;
     }
 }
